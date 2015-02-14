8a4bdd80ac2df2d66954126fe259158ad42da853
ellenlt <ellenltsay@gmail.com>
2015-02-13 18:28:52 -0800

    Delete all fo the garbage files.

--- Project5/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/app/models/photo.rb
+++
@@ -1,7 +0,0 @@
-class Photo < ActiveRecord::Base
-	belongs_to :user
-	has_many :comments
-	def num_comments
-		self.comments.count
-	end
-end
--- Project5/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/tsay@cardinal3.stanford.edu/app/models/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment < ActiveRecord::Base
-	belongs_to :photo
-	belongs_to :user
-
-	validates_presence_of :comment,
-		:message => "box cannot be empty."	
-end
